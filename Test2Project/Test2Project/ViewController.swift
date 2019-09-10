daeb944aca906a5c37ed739fde5189bd92a7ea97
AlekseyK <aleksey.kabishau@gmail.com>
2019-09-09 23:11:37 -0400

    commit

---
+++ Test2Project/Test2Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2Project
+//
+//  Created by Aleksey on 9/9/19.
+//  Copyright © 2019 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
